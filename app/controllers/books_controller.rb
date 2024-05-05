e54166dcf78a0fb349393ec68704654ca6e7161e
7suzuki <162399385+7suzuki@users.noreply.github.com>
2024-05-05 09:32:35 +0000

    [Add]book show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,37 +1,40 @@
 class BooksController < ApplicationController
 
   def new
-    book = Book.new
+    @book = Book.new
   end
-  
+
   def create #データの保存
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
-    redirect_to book_path
-  end
-  
-  def show
-    
-  end
-  
-  def index
-    
-  end
-  
-  def edit
-    
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id) #@bookにすることで今作ったbookになる
   end
   
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+
+  end
+
+  def edit
+     @book = Book.find(params[:id])
+  end
+
+
   # def destory  end
   # def update  end
-  
+
   private
-  
+
   def book_params
-    params.require(:book).permit(:book_name, :image, :caption)
-    
+    params.require(:book).permit(:title, :body, :user_id)
+
   end
-  
+
 end
