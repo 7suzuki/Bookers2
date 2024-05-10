2cc13a7220ba930678cd0a6eef11b52add9924ee
7suzuki <162399385+7suzuki@users.noreply.github.com>
2024-05-10 16:05:46 +0000

    [Add]layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,21 +4,42 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    # @post_images = @user.post_images #(全件取得)
+    # @post_images = @user.post_images.page(params[:page]) #ページネーション（取得数制限）
   end
   
   def index
+    @users = User.all
     
   end
   
-  def edit
+  # def edit
+  #   user = User.find(params[:id])
+  #   unless user.id == current_user.id
+  #     redirect_to post_images_path
+  #   end
+  #   @user = User.find(params[:id])
+  # end
+  
+  # def update
+  #   user = User.find(params[:id])
+  #   unless user.id == current_user.id
+  #     redirect_to post_images_path
+  #   end
     
-  end
-  
-  def update
-  
-  end
+  #   @user = User.find(params[:id])
+  #   @user.update(user_params)
+  #   redirect_to user_path(@user.id) #ユーザーの詳細ページに画面遷移
+  # end
   
   # def create  end
   # def destory  end
+  
+
+  private
+    
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
   
 end
