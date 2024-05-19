edd7992d908eadb48aa714ab4ad88748ba5b979d
7suzuki <162399385+7suzuki@users.noreply.github.com>
2024-05-19 13:53:04 +0000

    [Add]validate

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,7 @@ class BooksController < ApplicationController
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id) #@bookにすることで今作ったbookになる
     else
+      @books = Book.all
       render :index
     end
   end
@@ -41,9 +42,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
+    if @book.update(book_params)
     flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   private
